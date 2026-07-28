6370343553bf7aed62d73a36ff96fa36c077f31c
ZAIN-Cod <2025se405@student.uet.edu.pk>
2026-07-28 18:05:25 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ecommerce-vip
+Responsive E-commerce website built with React and Tailwind CSS
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center bg-gray-900 text-white px-6 py-4">
+        <h1 className='text-xl font-semibold'>My shop</h1>
+    <ul  className="flex gap-6  list-none">
+        <li className='cursor-pointer hover:text-blue-400 transition-duration-300'>Home</li>
+        <li className='cursor-pointer hover:text-blue-400 transition-duration-300'>Shop</li>
+        <li className='cursor-pointer hover:text-blue-400 transition-duration-300'>Cart</li>
+        </ul>
+        </nav>
+
+
+  )
+}
+
+export default Navbar
